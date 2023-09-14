e8f09de1ccd5336f20af55a6f86fd6123c0ccd31
VCrishna <vskrishna5558@gmail.com>
2023-09-14 11:26:15 -0400

    Fetch_data.yml added

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -5,17 +5,23 @@ import { environment } from '../environments/environment';
   providedIn: 'root',
 })
 export class GithubService {
-  baseUrl = 'https://api.github.com/repos/VCrishna/LeetCode/contents/';
+  // baseUrl = 'https://api.github.com/repos/VCrishna/LeetCode/contents/';
+  baseUrl = './data.json';
   private headers = new HttpHeaders({
     Authorization: `token ${environment.githubToken}`,
   });
 
   constructor(private http: HttpClient) {}
 
+  // getFileContent(path: string = '') {
+  //   console.log(environment.githubToken);
+  //   return this.http.get<any>(`${this.baseUrl}${path}`, {
+  //     headers: this.headers,
+  //     responseType: 'json',
+  //   });
+  // }
   getFileContent(path: string = '') {
-    console.log(environment.githubToken);
-    return this.http.get<any>(`${this.baseUrl}${path}`, {
-      headers: this.headers,
+    return this.http.get<any>(this.baseUrl, {
       responseType: 'json',
     });
   }
