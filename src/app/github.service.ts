15ce20aea30f1a7e82bedd2d344d06fe557d22c8
VCrishna <vskrishna5558@gmail.com>
2023-09-14 15:19:38 -0400

    Your commit message here

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -5,24 +5,24 @@ import { environment } from '../environments/environment';
   providedIn: 'root',
 })
 export class GithubService {
-  // baseUrl = 'https://api.github.com/repos/VCrishna/LeetCode/contents/';
-  baseUrl = './data.json';
+  baseUrl = 'https://api.github.com/repos/VCrishna/LeetCode/contents/';
+  // baseUrl = 'assets/data.json';
   private headers = new HttpHeaders({
     Authorization: `token ${environment.githubToken}`,
   });
 
   constructor(private http: HttpClient) {}
 
-  // getFileContent(path: string = '') {
-  //   console.log(environment.githubToken);
-  //   return this.http.get<any>(`${this.baseUrl}${path}`, {
-  //     headers: this.headers,
-  //     responseType: 'json',
-  //   });
-  // }
   getFileContent(path: string = '') {
-    return this.http.get<any>(this.baseUrl, {
+    console.log(environment.githubToken);
+    return this.http.get<any>(`${this.baseUrl}${path}`, {
+      headers: this.headers,
       responseType: 'json',
     });
   }
+  // getFileContent(path: string = '') {
+  //   return this.http.get<any>(this.baseUrl, {
+  //     responseType: 'json',
+  //   });
+  // }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  githubToken: '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',
+  // githubToken: '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',
+  githubToken: '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',
 };
