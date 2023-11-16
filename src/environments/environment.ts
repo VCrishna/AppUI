dbac5a5951638dbfebc341e7f69761c289a16a3d
VCrishna <vskrishna5558@gmail.com>
2023-11-16 18:44:52 -0500

    Committing changes before switching branches

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // githubToken: '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',
-  githubToken: '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',
+  githubToken: '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',
 };
